bb39916ac988bf456a3dd1ce8bd1bb848f7b6607
victoria <liuvictoria12@gmail.com>
2024-10-26 16:20:17 -0400

    everything

---
+++ base.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn import metrics
+from sklearn.svm import SVC
+from xgboost import XGBRegressor
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.ensemble import RandomForestRegressor
+
+import warnings
+warnings.filterwarnings('ignore')
+df1 = pd.read_csv(r"calories.csv")
+df2 = pd.read_csv(r"exercise.csv")
+
+df = pd.concat([df1, df2], axis=1)
+df = df.loc[:, ~df.columns.duplicated()]
+df.head()
+
+sb.scatterplot(x='Height', y = 'Weight', data=df)
+plt.show
+
+features = ['Age', 'Height', 'Weight', 'Duration']
+plt.subplots(figsize=(15,10))
+for i, col in enumerate(features):
+    plt.subplot(2, 2, i+1)
+    x = df.sample(1000)
+    sb.scatterplot(x=col, y="Calories", data=x)
+plt.tight_layout()
+plt.show
+
+features = df.select_dtypes(include='float').columns
+plt.subplots(figsize=(15,10))
+for i, col in enumerate(features):
+    plt.subplot(2, 3, i+1)
+    sb.distplot(df[col])
+plt.tight_layout()
+plt.show()
+
+df.replace({'male': 0, 'female':1}, inplace=True)
+df.head()
+
+plt.figure(figsize=(8,8))
+sb.heatmap(df.corr()>0.9, annot=True, cbar=False)
+plt.show
+
+to_remove = ["Weight", "Duration"]
+df.drop(to_remove, axis=1, inplace=True)
+
+features = df.drop(["User_ID", "Calories"], axis=1)
+target = df['Calories'].values
+
+X_train, X_val, Y_train, Y_val = train_test_split(features, target, test_size=0.1, random_state=22)
+X_train.shape, X_val.shape
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_val = scaler.transform(X_val)
+
+from sklearn.metrics import mean_absolute_error as mae
+
+models = [LinearRegression(), XGBRegressor(), Lasso(), RandomForestRegressor(), Ridge()]
+
+for i in range(5):
+    models[i].fit(X_train, Y_train)
+
+    print(f'{models[i]} : ')
+
+    train_preds = models[i].predict(X_train)
+    print('Training Error: ', mae(Y_train, train_preds))
+
+    val_preds = models[i].predict(X_val)
+    print('Validation Error: ', mae(Y_val, val_preds))
+    print()
